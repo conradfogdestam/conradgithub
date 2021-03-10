daf1d84e1c5f90b5fb691c2b4eb58eb6a06d02af
conradfogdestam <70263566+conradfogdestam@users.noreply.github.com>
2021-03-10 22:38:30 +0100

    donnerz

---
+++ Programmering 1/Dictionary/Dictionary01.py
@@ -0,0 +1,14 @@
+kurser dict(
+    tillämpad_1 = 100,
+    programmering_1 = 100,
+    daodac = 100,
+    web_1 = 100,
+    svenska_2 = 100,
+    fysik_1 = 150,
+    idrott = 100,
+    engelska_6 = 100,
+    matematik_4 = 100
+)
+
+for i in kurser:
+
--- Tillempadprogrammering 1/Moment05/main.py
+++ Tillempadprogrammering 1/Moment05/main.py
@@ -1,26 +1,30 @@
 #kursolle moment05 bankapp
 #Conrad Fogdestam te19d
 
-from meee import bankAccount # funktion för .balance .withdraw .deposit
 import os # för att kunna wipea alla filer
+import FUNKTIONER
 import sys # stänga programmet vid EXIT
 from colorama import init #färgtext
 from colorama import Fore #färgtext
 init(autoreset=True) #färgtext reset efter varje print
 
-file_trans = 'transactions.txt'
 file_users = 'accountfile.txt'
-file_bal = 'balance.txt'
-usern = ['NOT LOGIN']
-baluser = [1000]
+logged_in_user = []
+logged_in_password = []
 
 
 def register():
-    print(Fore.LIGHTMAGENTA_EX + 'Input credentials to register account')
-    username = input(Fore.CYAN + "Username ")
-    usern.append(username)
-    usern[-1] = bankAccount(username, 1000)
-    password = input(Fore.CYAN + "Password ")
+    print(Fore.LIGHTMAGENTA_EX + 'Input credentials to register account: ')
+    username = input(Fore.CYAN + "Username: ")
+    password = input(Fore.CYAN + "Password: ")
+    with open(username + 'transactions.txt', 'a+') as f:
+        f.write('Balance $1000\n')
+    with open(username + 'profile.txt', 'w+') as f:
+        f.write(username)
+        f.write(" ")
+        f.write(password)
+        f.write(" ")
+        f.write('1000')
     with open(file_users, "a") as f:
         f.write(username)
         f.write(" ")
@@ -28,14 +32,14 @@ def register():
         f.write("\n")
         f.close()
         print(Fore.LIGHTMAGENTA_EX + f'Account set up, thank you for choosing CAYMAN ISLANDS NATIONAL BANK™')
-    with open(file_bal, 'w+') as fi:
-        fi.write(str(1000))
+
 
 def login():
     print(Fore.LIGHTMAGENTA_EX + 'To login please input your credentials')
-    username = input(Fore.CYAN + "Username ")
-    password = input(Fore.CYAN + "Password ")
-    usern.append(username)
+    username = input(Fore.CYAN + "Username: ")
+    password = input(Fore.CYAN + "Password: ")
+    logged_in_user.append(username)
+    logged_in_password.append(password)
     for line in open(file_users, "r").readlines():
         login_info = line.split()
         if username == login_info[0] and password == login_info[1]:
@@ -63,66 +67,64 @@ while True:
 
     if userinput == 'L':
             if login() == True:
-                with open(file_bal, 'r') as f:
-                    temp = f.read()
-                    baluser.append(int(temp))
-
-                print(baluser)
-                print(usern)
-
-                user1 = bankAccount(usern[-1], int(baluser[-1]))
 
                 while True:
+
+                    user = (str(logged_in_user).join(logged_in_user))
+                    password = (str(logged_in_password).join(logged_in_password))
+
                     userinput = input(Fore.LIGHTCYAN_EX + 'CAYMAN ISLANDS NATIONAL BANK™\n D) Make deposit\n W) Make withdrawal\n T) Check transactions\n B) Check available balance\n E) EXIT\nEnter "TERMINATE" to delete all data\n').upper()
 
                     if userinput == 'W':
 
                         ui = int(input(Fore.LIGHTMAGENTA_EX + 'How much would you like to withdraw?'))
-                        if ui < user1.balance:
-                            user1.withdraw(ui)
+                        if ui < FUNKTIONER.check_balance(str(user)):
+                            print('gg')
+                            FUNKTIONER.withdrawal(user, password, ui)
                             print(Fore.LIGHTMAGENTA_EX + '${} withdrawn from account '.format(ui))
-                            with open(file_trans, 'a+') as f:
-                                f.write(f'- ${ui}, Available balance: $' + str(user1.balance) + '\n')
-                            with open(file_bal, 'w+') as fi:
-                                fi.write(str(user1.balance))
+                            with open(str(user) + 'transactions.txt', 'a+') as f:
+                                f.write(f'- ${ui}, Available balance: $' + str(FUNKTIONER.check_balance(user)) + '\n')
 
                         else:
                             print('Insufficient funds')
-                            print('Funds currently available to withdraw', user1.balance)
+                            print('Funds currently available to withdraw', FUNKTIONER.check_balance(str(user)))
+
 
                     elif userinput == 'D':
                         try:
                             uis = int(input(Fore.LIGHTMAGENTA_EX + 'How much would you like to deposit?'))
-                            user1.deposit(uis)
+                            FUNKTIONER.deposit(user, password, uis)
                             print(Fore.LIGHTMAGENTA_EX + '${} deposited to account'.format(uis))
-                            with open(file_trans, 'a+') as f:
-                                f.write(f'+ ${uis}, Available balance: $' + str(user1.balance) + '\n')
-                            with open(file_bal, 'w+') as fi:
-                                fi.write(str(user1.balance))
+                            with open(str(user) + 'transactions.txt', 'a+') as f:
+                                f.write(f'+ ${uis}, Available balance: $' + str(FUNKTIONER.check_balance(user)) + '\n')
+
+
                         except:
                             print(Fore.LIGHTRED_EX + 'Deposit needs to be an integer')
 
                     elif userinput == 'T':
-                        try:
-                            with open(file_trans, 'r+') as f:
-                                for line in file_trans:
-                                    print(f.readline().strip('\n'))
-                        except:
-                            print('No Transactions to display')
+
+                        with open(str(user) + 'transactions.txt', 'r+') as f:
+                            for line in (str(user) + 'transactions.txt'):
+                                print(f.read())
+                                #print(f.readlines().strip('\n'))
+
 
                     elif userinput == 'B':
-                        print(Fore.LIGHTMAGENTA_EX + "$", user1.balance)
+                        print(FUNKTIONER.check_balance(str(user)))
 
                     elif userinput == 'E':
                         print(Fore.LIGHTMAGENTA_EX + 'GOODBYE')
                         sys.exit()
 
                     elif userinput == 'TERMINATE':
-
-                        os.remove('accountfile.txt')
-                        os.remove('transactions.txt')
-                        os.remove('balance.txt')
-                        baluser.clear()
-                        usern.clear()
+                        os.remove(str(user) + 'profile.txt')
+                        os.remove(str(user) + 'transactions.txt')
+                        with open(file_users, "r") as f:
+                            lines = f.readlines()
+                        with open(file_users, "w") as f:
+                            for line in lines:
+                                if line.strip("\n") != str(user) + ' ' + str(password):
+                                    f.write(line)
                         print(Fore.LIGHTMAGENTA_EX + 'Sucsessfully deleted all files')
                         sys.exit()
