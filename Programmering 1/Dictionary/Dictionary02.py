5ecc610c17800dad5ae958bbd43390b548cf49c5
conradfogdestam <70263566+conradfogdestam@users.noreply.github.com>
2021-03-11 17:30:10 +0100

    yeet

--- Programmering 1/Dictionary/Dictionary02.py
+++ Programmering 1/Dictionary/Dictionary02.py
@@ -1,6 +1,6 @@
 import random as rnd
 
-amount = {
+amount = { # dictionary för att hålla info om antal tärningskast på olika nummer
     '1': 0,
     '2': 0,
     '3': 0,
@@ -9,10 +9,10 @@ amount = {
     '6': 0,
 }
 
-for i in range(100000):
+for i in range(100000): # loopar 100000 gånger random mellan 1-6
     kast = rnd.randint(1,6)
-    amount[str(kast)] += 1
+    amount[str(kast)] += 1 # gör random talet till en sträng och lägger till 1 i motsvaande rad i dictionary
 
-print('Frekvenstabell')
-for key, value in amount.items():
+print('Amount of rolls')
+for key, value in amount.items(): # printar allt ur dictionary 
     print(f"{key}: {value}")
