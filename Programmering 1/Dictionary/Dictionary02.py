c0cfae9b1cca9448ef6cba79aabaffb751a31538
conradfogdestam <70263566+conradfogdestam@users.noreply.github.com>
2021-03-17 12:11:03 +0100

    dd

--- Programmering 1/Dictionary/Dictionary02.py
+++ Programmering 1/Dictionary/Dictionary02.py
@@ -12,6 +12,7 @@ amount = { # dictionary för att hålla info om antal tärningskast på olika nu
     '6': 0,
 }
 
+
 for i in range(100000): # loopar 100000 gånger random mellan 1-6
     kast = rnd.randint(1,6)
     amount[str(kast)] += 1 # gör random talet till en sträng och lägger till 1 i motsvaande rad i dictionary
