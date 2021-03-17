4db94208956b7da3a7ab4a7a3a6f1a4f12b1e919
conradfogdestam <70263566+conradfogdestam@users.noreply.github.com>
2021-03-17 12:45:44 +0100

    d

---
+++ Programmering 1/Numpy/Numpy02.py
@@ -0,0 +1,5 @@
+import  numpy as np
+
+doink = np.arange(10, 110, 10)
+
+print(doink)
---
+++ Programmering 1/Numpy/Numpy03.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(1,11)
+y = x*3
+
+plt.plot(x,y)
+plt.ylabel
+plt.xlabel
+plt.show
+print(y)
